6c8d62cf0629afbabb5d6b4436909909e4a9b50c
Rai <56082069+raicool@users.noreply.github.com>
2026-04-16 20:23:04 -0400

    create leaderboard page dynamically from lb script

--- battle_summary.py
+++ battle_summary.py
@@ -16,22 +16,32 @@ class summary:
 
 	left_uuid: uuid.uuid4
 	left_name: str
+	left_elo_original: float
+	left_elo_final: float
 	left_elo_delta: float
+	left_rank_original: int
+	left_rank_final: int
 	left_rank_delta: int
 
 
 	right_uuid: uuid.uuid4
 	right_name: str
+	right_elo_original: float
+	right_elo_final: float
 	right_elo_delta: float
+	right_rank_original: int
+	right_rank_final: int
 	right_rank_delta: int
 
 	winner_side: int
 
 	def __init__(self, left: trainer, right: trainer):
-		self.left_elo_delta = left.elo
-		self.left_rank_delta = left.rank
-		self.right_elo_delta = right.elo
-		self.right_rank_delta = right.rank
+		self.left_elo_original = left.elo
+		self.left_rank_original = left.rank
+
+		self.right_elo_original = right.elo
+		self.right_rank_original = right.rank
+
 		self.left_uuid = left.id
 		self.left_name = left.name
 		self.right_uuid = right.id
@@ -39,10 +49,16 @@ class summary:
 	
 	def finalize(self, left: trainer, right: trainer, side: int):
 		self.timestamp = datetime.now().timestamp()
-		self.left_elo_delta -= left.elo
-		self.left_rank_delta -= left.rank
-		self.right_elo_delta -= right.elo
-		self.right_rank_delta -= right.rank
+		self.left_elo_final = left.elo
+		self.left_elo_delta = left.elo - self.left_elo_original
+		self.left_rank_final = left.rank
+		self.left_rank_delta = left.rank - self.left_rank_original
+
+		self.right_elo_final = right.elo
+		self.right_elo_delta = right.elo - self.right_elo_original
+		self.right_rank_final = right.rank
+		self.right_rank_delta = right.rank - self.right_rank_original
+
 		self.winner_side = side
 
 		self.serialize()
@@ -62,18 +78,26 @@ class summary:
 			"timestamp": self.timestamp,
 			"left_uuid": self.left_uuid,
 			"left_name": self.left_name,
+			"left_elo_original": self.left_elo_original,
+			"left_elo_final": self.left_elo_final,
 			"left_elo_delta": self.left_elo_delta,
+			"left_rank_original": self.left_rank_original,
+			"left_rank_final": self.left_rank_final,
 			"left_rank_delta": self.left_rank_delta,
 			"right_uuid": self.right_uuid,
 			"right_name": self.right_name,
+			"right_elo_original": self.right_elo_original,
+			"right_elo_final": self.right_elo_final,
 			"right_elo_delta": self.right_elo_delta,
+			"right_rank_original": self.right_rank_original,
+			"right_rank_final": self.right_rank_final,
 			"right_rank_delta": self.right_rank_delta,
 			"winner_side": self.winner_side
 		})
 
 		json.dump(log_list, battle_log)
 		battle_log.close()
-		shutil.copy(SUMMARY_FILE, "website/html/summary.json")
+		shutil.copy(SUMMARY_FILE, "website/summary.json")
 
 
 if __name__ == "__main__":
--- website/leaderboard.js
+++ website/leaderboard.js
@@ -1,4 +1,7 @@
-import { read_file, comparer, len, timestamp_diff } from "./utility.js";
+import { read_file, comparer, pokemon, timestamp_diff } from "./utility.js";
+
+const BOX_SPRITE_URL = "https://raw.githubusercontent.com/msikma/pokesprite/master/pokemon-gen7x/"
+const LEAGUE_SPRITE_URL = "https://raw.githubusercontent.com/msikma/pokesprite/master/items/ball/"
 
 var selected_header
 
@@ -27,6 +30,29 @@ function add_cell_text(row, text)
 	// Append a text node to the cell
 	const node = document.createTextNode(text);
 	cell.appendChild(node);
+    return node;
+}
+
+function add_cell_image(row, src, _class)
+{
+	const cell = row.insertCell();
+
+	// Append a text node to the cell
+	const img = document.createElement("img");
+    img.setAttribute("src", src);
+    img.setAttribute("class", _class);
+	cell.appendChild(img);
+    return img;
+}
+
+function add_cell_element(row, element_tag)
+{
+	const cell = row.insertCell();
+
+	// Append a text node to the cell
+	const e = document.createElement(element_tag);
+	cell.appendChild(e);
+    return e;
 }
 
 function read_dump_data()
@@ -34,39 +60,22 @@ function read_dump_data()
 	read_file("trainers.json", function(file)
 		{
 			var data = JSON.parse(file);
-			var change = false
 
 			var table = document.getElementById("leaderboard");
+
 			var i = 0;
-			for (const [key, value] of Object.entries(data))
-			{
-				var row = document.getElementById("pidx" + i);
-
-				if (value.hasOwnProperty("last_match"))
-				{
-					row.children[0].innerHTML = timestamp_diff(value.last_match)
-				}
-
-				if (value.hasOwnProperty("rank"))
-				{
-					if (row.children[1].innerHTML != value.rank)
-					{
-						row.children[1].innerHTML = value.rank;
-						change = true
-					}
-				}
-
-				if (value.hasOwnProperty("elo"))
-				{
-					row.children[12].innerHTML = Math.round(value.elo);
-				}
-
-				i++;
-			}
+            if (ready)
+            {
+                update_trainer_rows(data)
+            }
+            else
+            {
+                setup_trainer_rows(table, data)
+            }
 			
-			if (selected_header && change)
+			if (selected_header)
 			{
-				sort_table(table, false)
+				sort_table(table, false);
 			}
 		}
 	)
@@ -75,31 +84,112 @@ function read_dump_data()
 		{
 			var data = JSON.parse(file);
 
-			var table = document.getElementById("battle-log")
+			var table = document.getElementById("battle-log");
 			if (table)
 			{
-				var body = table.tBodies[0]
+				var body = table.tBodies[0];
 				
-				body.innerHTML = ""
+				body.innerHTML = "";
 
-				for (const [key, value] of Object.entries(data).reverse())
+				for (const [, value] of Object.entries(data).reverse())
 				{
-					var row = body.insertRow()
+					var row = body.insertRow();
 					var date = new Date(value.timestamp * 1000);
-					add_cell_text(row, date.toLocaleTimeString())
-					add_cell_text(row, value.left_name)
-					add_cell_text(row, value.winner_side == 1 ? "W" : "L")
-					add_cell_text(row, Math.round(value.left_elo_delta))
-					add_cell_text(row, value.left_rank_delta)
+					add_cell_text(row, date.toLocaleTimeString());
+					add_cell_text(row, value.left_name);
+					add_cell_text(row, value.winner_side == 1 ? "W" : "L");
+					add_cell_text(row, `${Math.round(value.left_elo_final)} (${Math.round(value.left_elo_delta)})`);
+					add_cell_text(row, value.left_rank_delta);
 
-					add_cell_text(row, value.right_name)
-					add_cell_text(row, value.winner_side == 2 ? "W" : "L")
-					add_cell_text(row, Math.round(value.right_elo_delta))
-					add_cell_text(row, value.right_rank_delta)
+					add_cell_text(row, value.right_name);
+					add_cell_text(row, value.winner_side == 2 ? "W" : "L");
+					add_cell_text(row, `${Math.round(value.right_elo_final)} (${Math.round(value.right_elo_delta)})`);
+					add_cell_text(row, value.right_rank_delta);
 				}
 			}
 		}
 	)
 }
 
-read_dump_data()
+function update_trainer_rows(data)
+{
+    var i = 0;
+    for (const [key, value] of Object.entries(data))
+    {
+        const row = document.getElementById("pidx" + i);
+        if (value.hasOwnProperty("last_match"))
+        {
+            row.children[0].innerHTML = timestamp_diff(value.last_match)
+        }
+
+        if (value.hasOwnProperty("rank"))
+        {
+            if (row.children[1].innerHTML != value.rank)
+            {
+                row.children[1].innerHTML = value.rank;
+                change = true
+            }
+        }
+
+        if (value.hasOwnProperty("elo"))
+        {
+            row.children[12].innerHTML = Math.round(value.elo);
+        }
+
+        i++;
+    }
+}
+
+var ready = false;
+function setup_trainer_rows(table, data)
+{
+    var body = table.tBodies[0];
+	body.innerHTML = "";
+
+    var i = 0;
+    for (const [, value] of Object.entries(data))
+    {
+        var row = body.insertRow();
+        row.id = "pidx" + i;
+
+        add_cell_text(row, timestamp_diff(value.last_match));
+
+        add_cell_text(row, value.rank);
+
+        var trainer_img = add_cell_image(row, `sprites/mugshot/${value.trainer_pic}.png`, "mugshot");
+        trainer_img.setAttribute("class", "mugshot");
+
+        var trainer_name = add_cell_element(row, "a");
+        trainer_name.innerHTML = value.name;
+        trainer_name.setAttribute("href", `player.html?id=${value.id}`)
+
+        add_cell_image(row, LEAGUE_SPRITE_URL + `${value.league}.png`);
+
+        var tid = add_cell_element(row, "code");
+        tid.innerHTML = value.id;
+        tid.setAttribute("class", "code");
+
+        const party = value.party;
+        for (const pkmn of party)
+        {
+            var img = add_cell_image(
+                row, 
+                `${BOX_SPRITE_URL}${pkmn.shiny ? "shiny/" : "regular/"}${pokemon[pkmn.species].toLowerCase()}.png`
+            );
+            img.setAttribute("class", "p");
+            img.parentNode.setAttribute("class", "image");
+        }
+
+        add_cell_text(row, Math.round(value.elo));
+        const wl_ratio = value.wins / (value.wins + value.losses);
+        add_cell_text(row, value.wins);
+        add_cell_text(row, value.losses);
+        add_cell_text(row, Math.round(100 * wl_ratio) + "%");
+        add_cell_text(row, value.battles);
+
+        i++;
+    }
+    ready = true;
+}
+
+read_dump_data();
